faa8ed1fc66f679b0a36c34f3a5e2cbb9f165abb
Daniel Fan (Student) <748176@learn.vsb.bc.ca>
2022-02-09 14:44:00 -0800

    Upload IntelliJ Project with classes; PDF question Answers

---
+++ InheritanceAssignment1/src/Planet.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class Planet {
+    private int orbitTime;
+    private ArrayList<Moon> moons;
+    private String designation;
+
+    public Planet() {
+    }
+
+    public Planet(int orbitTime, String designation) {
+        this.orbitTime = orbitTime;
+        this.designation = designation;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getOrbitTime() {
+        return orbitTime;
+    }
+
+    public void setOrbitTime(int orbitTime) {
+        this.orbitTime = orbitTime;
+    }
+
+    public void addMoon(Moon moon){
+        this.moons.add(moon);
+    }
+
+    public ArrayList<Moon> getMoons() {
+        return moons;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
+//pdf skip 1
