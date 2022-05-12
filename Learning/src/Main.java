46e06ea84f4e008c1bbb804b4482e0bfddd821a2
DanielFanUHS <99220584+DanielFanUHS@users.noreply.github.com>
2022-05-12 13:35:58 -0700

    update progress

--- Learning/src/Main.java
+++ Learning/src/Main.java
@@ -1,31 +1,17 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        HashSet<String> words = new HashSet<>();
-        Scanner scanner = new Scanner(new File("illiad.txt"));
-
-        while(scanner.hasNext()){
-            //remove all characters(eg. punctuation) besides letters and numbers -> lowercase
-            String word = scanner.next().replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-            if (word.length() > 1 || word == "a" || word == "i"){
-                words.add(word);
-            }
-        }
-
-        //System.out.println(words);
-        System.out.println(words.size() + " unique words");
-        // one map z
-        //second map
-        //use strings not char(more method)
-        //in constructor create a map with key mapped to shifted value
+    public static void main(String[] args) {
 
     }
+
+    public static ArrayList<Integer> merge(ArrayList<Integer> arrayA, ArrayList<Integer> arrayB){
+        ArrayList<Integer> arrayC = new ArrayList<>();
+        while(arrayA.size() > 0 || arrayB.size() > 0){
+            if(arrayA.get(0) == arrayB.get(0)){
+
+            }
+        }
+    }
 }
-
-
-
-
+//use archetypes: apache derby and appache commons file io to create the proejct
